457fa1fc18089870ea0b1c8bf9f87e53ffcc9771
Simon Zimmerman <simon@mysolomon.co.uk>
2021-11-25 20:40:28 +0000

    feat: enable embroider options - breaks the build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,5 +22,10 @@ module.exports = function (defaults) {
 
   // return app.toTree();
   const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack);
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticComponents: true,
+  });
 };
